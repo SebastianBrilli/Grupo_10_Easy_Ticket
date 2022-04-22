9b50ffd18c4f11ba54cd0d0f1398f4e192115285
Manuel Jacubowicz <manu_jacu@hotmail.com>
2022-04-21 21:42:25 -0300

    Actualizando

--- app.js
+++ app.js
@@ -12,10 +12,8 @@ app.listen( process.env.PORT || 3001,()=>{
 app.get("/",(req,res)=>{
     res.sendFile(path.resolve(__dirname,"./views/home.html"))
 });
-app.get("/register",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"./views/register.html"))
+app.get("/carro",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"./views/carroDeCompras.html"))
 });
-app.get("/login",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"./views/login.html"))
-});
+
 
