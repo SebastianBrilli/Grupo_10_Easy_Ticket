fbbec98eea43280ac8282c7520e6769f9a575c1d
Sebastian Brilli <sebastianbrilli@MacBook-Pro-de-Sebastian.local>
2022-04-30 19:37:31 -0300

    Pasando los eventos por param a la home

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,8 +1,21 @@
+let fs = require('fs');
+const path = require('path');
+
+const eventFilePath = path.join(__dirname, '../data/eventDataBase.json');
+const event = JSON.parse(fs.readFileSync(eventFilePath, 'utf-8'));
+
+
 
 const homeController={
 
     index: (req,res)=>{
-    res.render("home")
+        const buenosAires = event.filter(elemento => elemento.category === 'Buenos Aires');
+        const cordoba = event.filter(elemento => elemento.category === 'Cordoba');
+        const rosario = event.filter(elemento => elemento.category === 'Rosario');
+        const otrasProvincias = event.filter(elemento => elemento.category === 'Otras Provincias');
+		
+
+    res.render("home",{buenosAires,cordoba,rosario,otrasProvincias})
     }
 
 }
